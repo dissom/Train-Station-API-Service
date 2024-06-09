91e7ab20f86ee019db3840bef3a3e2a9720a1de4
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-06-09 22:35:33 +0300

    deleted unnecessary database

--- train_service/settings.py
+++ train_service/settings.py
@@ -101,12 +101,7 @@ DATABASES = {
         "PORT": os.environ["POSTGRES_PORT"],
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
